d8711b4c6a9b62ca9dfbc69daf51ea35b08186dd
franksants <franklinjuansantos@gmail.com>
2026-01-23 10:44:53 -0300

    Add variables project.

---
+++ variables/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+
+    let variavel_testes: u16 = 0xAFF;
+
+
+
+    let testing: i8 = 0x0A;
+
+    println!("Working right now: {}, {}", testing, variavel_testes);
+
+    let mut x=10;
+    println!("Value of X is {}", x);
+    
+    x=5;
+    println!("Value of X is {}", x);
+}
