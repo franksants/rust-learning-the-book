d0ef2bbdb15fe85fcf5223a482eed8dfc272b0cb
franksants <franklinjuansantos@gmail.com>
2026-01-26 13:29:53 -0300

    Add function project

---
+++ functions/src/main.rs
@@ -0,0 +1,17 @@
+//use std::io;
+fn main() {
+    let y: u32 = {
+        let x = 10;
+        let b = 20;
+        b + x
+    };
+    println!("x + b = {}", y);
+    //    println!("Hello, world!");
+    //    let mut input = String::new();
+    //    io::stdin().read_line(&mut input).expect("Invalid argument");
+    //    another_function(&mut input);
+    //    println!("The input value is: {input}");
+}
+//fn another_function(x: &mut String) {
+//    *x += "deci davi, davi deci";
+//}
